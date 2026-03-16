0ec9587cbbef0c04f1a18dcf077ef13e4e8d3c52
Yasar195 <imyasar07@gmail.com>
2026-03-16 12:12:38 +0530

    fix project slug

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
-    println!("penny");
-    println!("Hello, world!");
+use rocket::{get, launch, routes};
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocker() -> _ {
+    rocket::build().mount("/", routes![index])
 }
